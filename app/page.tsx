acb349fc20ee436f5115b296dec9d98a28634502
Thi Anh Thi Nguyen <thi.miranda348@gmail.com>
2026-06-04 10:13:19 -0600

    feat: update the style colors and font

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-24 bg-slate-50 dark:bg-slate-950">
-      <h1 className="text-4xl font-bold text-slate-900 dark:text-white">
+    <main className="">
+      <h1 className="text-2xl font-bold text-primary">
         LabInsight AI
       </h1>
+      
     </main>
   );
 }
